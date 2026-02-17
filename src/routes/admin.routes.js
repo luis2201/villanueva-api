ae6de53868d7db0691496a9a5b16dafc32d9b8ac
unknown <lupin2201@gmail.com>
2026-02-16 23:56:45 -0500

    Estableciendo rutas para pages

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,18 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-// Rutas para administración de páginas (CRUD)
-const auth = require('../middlewares/auth.middleware');
-const allowRoles = require('../middlewares/role.middleware');
-const pageController = require('../controllers/pages.controller');
+const pagesRoutes = require('./pages.routes');
 
-// lectura (todos autenticados)
-router.get('/', auth, allowRoles(['ADMIN','EDITOR','PUBLISHER','VIEWER']), pageController.list);
-router.get('/:id', auth, allowRoles(['ADMIN','EDITOR','PUBLISHER','VIEWER']), pageController.getById);
-
-// escritura
-router.post('/', auth, allowRoles(['ADMIN','EDITOR','PUBLISHER']), pageController.create);
-router.put('/:id', auth, allowRoles(['ADMIN','EDITOR']), pageController.update);
-router.delete('/:id', auth, allowRoles(['ADMIN']), pageController.remove);
+// aquí iremos agregando más módulos luego: posts, menus, media, etc.
+router.use('/pages', pagesRoutes);
 
 module.exports = router;
---
+++ src/routes/pages.routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../middlewares/auth.middleware');
+const allowRoles = require('../middlewares/role.middleware');
+const pageController = require('../controllers/page.controller');
+
+// lectura (todos autenticados)
+router.get('/', auth, allowRoles(['ADMIN','EDITOR','PUBLISHER','VIEWER']), pageController.list);
+router.get('/:id', auth, allowRoles(['ADMIN','EDITOR','PUBLISHER','VIEWER']), pageController.getById);
+
+// escritura
+router.post('/', auth, allowRoles(['ADMIN','EDITOR','PUBLISHER']), pageController.create);
+router.put('/:id', auth, allowRoles(['ADMIN','EDITOR']), pageController.update);
+router.delete('/:id', auth, allowRoles(['ADMIN']), pageController.remove);
+
+module.exports = router;
